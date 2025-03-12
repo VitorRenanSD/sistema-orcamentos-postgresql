b06939a396db2d6cb46e5693789f30388a4e4cbc
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-12 16:31:25 -0300

    Utilizando .env na conexão do banco

--- src/main/java/org/vitorrenansd/ConnectionDB.java
+++ src/main/java/org/vitorrenansd/ConnectionDB.java
@@ -4,16 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class ConnectionDB {
+     // Carrega o .env automaticamente
+    private static final Dotenv dotenv = Dotenv.load();
+
+    // Tenta a conexao ao banco
     public Connection getConnection() {
-        Connection  connection = null;
+        Connection connection = null;
 
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/sistema_orcamento", "postgres", "password");
+            String url = dotenv.get("DB_URL");
+            String user = dotenv.get("DB_USER");
+            String password = dotenv.get("DB_PASSWORD");
+            connection = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao conectar ao banco de dados: " + e.getMessage());
         }
         return connection;
-        
     }
 }
