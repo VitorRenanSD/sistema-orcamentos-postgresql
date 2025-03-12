02cb824b85f01d12d0f67aecf9fba95a8e93182d
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-12 16:30:52 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Conexao com PostgreSQL
     implementation("org.postgresql:postgresql:42.7.5")
+    // Leitura de arquivo .env
+    implementation("io.github.cdimascio:java-dotenv:5.2.2")
 }
 
 tasks.test {
