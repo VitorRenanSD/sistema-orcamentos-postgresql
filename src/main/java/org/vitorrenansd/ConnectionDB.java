c2c307c86b292a58010803678b0562783b2074c6
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-12 16:21:16 -0300

    Update ConnectionDB.java

--- src/main/java/org/vitorrenansd/ConnectionDB.java
+++ src/main/java/org/vitorrenansd/ConnectionDB.java
@@ -7,12 +7,13 @@ import java.sql.SQLException;
 public class ConnectionDB {
     public Connection getConnection() {
         Connection  connection = null;
+
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/sistema_orcamento",
-                    "postgres", "password");
+            connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/sistema_orcamento", "postgres", "password");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return connection;
+        
     }
 }
